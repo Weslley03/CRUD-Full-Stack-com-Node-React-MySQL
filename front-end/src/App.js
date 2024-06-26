ae66ca5efde5f86389d44cfe517dc23efe097bbd
oweslley03 <wesleymoraeswv03@hotmail.com>
2024-06-26 14:00:37 -0300

    front e exibição

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import Global from "./Styles/GlobalStyles";
+import styled from "styled-components";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Form from "./Components/Form.js";
+import Grid from "./Components/Grid.js";
+import { useEffect, useState } from "react";
+import axios from 'axios'
+
+const Container = styled.div`
+  width: 100%;
+  max-width: 800px;
+  margin-top: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+`;
+
+const Title = styled.h2``;
 
 function App() {
+
+  const [ users, setUsers ] = useState([]);
+  //const [ onEdit, setOnEdit ] = useState(null);
+
+  const getUsers = async () => {
+    try {
+      const res = await axios.get("http://localhost:8800");
+      console.log(res.data)
+      setUsers(res.data.sort((a, b) => (a.nomeUsers > b.nomeUsers ? 1 : -1)));
+    } catch (error) {
+      toast.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getUsers();
+  }, [setUsers])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <Container>
+      <Title>USUÁRIOS</Title>
+      <Form />
+      <Grid users={users}/>
+    </Container> 
+    <ToastContainer autoClose={3000} position='toast.POSITION.BOTTOM_LEFT'/>
+      <Global />
+    </>
+  )
 }
 
 export default App;
---
+++ front-end/src/Components/Grid.js
@@ -0,0 +1,83 @@
+import React from "react";
+import styled from "styled-components";
+//import axios from 'axios'
+import { FaTrash, FaEdit } from "react-icons/fa";
+//import { toast } from "react-toastify";
+
+const Table = styled.table`
+  width: 100%;
+  background-color: #fff;
+  padding: 20px;
+  box-shadow: 0px 0px 5px #ccc;
+  border-radius: 5px;
+  max-width: 1120px;
+  margin: 20px auto;
+  word-break: break-all;
+`;
+
+const Tr = styled.tr``;
+
+const Th = styled.th`
+  text-align: start;
+  border-bottom: inset;
+  padding-bottom: 5px;
+
+  @media (max-width: 500px) {
+    ${(props) => props.onlyWeb && "display: none"}
+  }
+`;
+
+export const Td = styled.td`
+  padding-top: 15px;
+  text-align: ${(props) => (props.alignCenter ? "center" : "start")};
+  width: ${(props) => (props.width ? props.width : "auto")};
+
+  @media (max-width: 500px) {
+    ${(props) => props.onlyWeb && "display: none"}
+  }
+`;
+
+const Thead = styled.thead``;
+const Tbody = styled.tbody``;
+
+function Grid({ users }) {
+
+    if(!users){
+        return <p>carregando...</p>
+    }
+  return (
+    <>
+      <Table>
+        <Thead>
+          <Tr>
+            <Th>Nome</Th>
+            <Th>Email</Th>
+            <Th onlyWeb>Fone</Th>
+            <Th></Th>
+            <Th></Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {users.map((user, i) => (
+            <Tr key={i}>
+              <Td width="30%"> {user.nameUsers} </Td>
+              <Td width="30%"> {user.emailUsers} </Td>
+              <Td width="20%" onlyWeb>
+                {" "}
+                {user.fone}{" "}
+              </Td>
+              <Td alignCenter width="5%">
+                <FaEdit />
+              </Td>
+              <Td alignCenter width="5%">
+                <FaTrash />
+              </Td>
+            </Tr>
+          ))}
+        </Tbody>
+      </Table>
+    </>
+  );
+}
+
+export default Grid;
